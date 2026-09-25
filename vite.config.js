b345dc05a856697d8670797be5083eea7179dc14
Voltsat <net71727@gmail.com>
2026-09-25 22:38:12 +0700

    ya

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Portfolio/',
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./src"),
-    },
-  },
 })
